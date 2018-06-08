0cd83929d7607df9fc84989d53d406a9ce8f9c76
jesseflores512 <jesseflores512@gmail.com>
2018-06-08 03:43:25 -0500

    heroku deploy

--- gulpfile.js
+++ gulpfile.js
@@ -52,14 +52,6 @@ gulp.task('webpack', (cb) => {
       cb(err);
     });
 })
-
-gulp.task('serveprod', function() {
-  connect.server({
-    root: [your_project_path],
-    port: process.env.PORT || 5000, // localhost:5000
-    livereload: false
-  });
-});
 // gulp.task('webpack', shell.task([
 //   'webpack'
 // ]))
